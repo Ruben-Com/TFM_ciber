04499c4e24835c472bd775ec4313bc53cae0950b
Ruben <ruben.comeron@edu.uah.es>
2024-08-15 18:09:49 +0200

    Mains con temporización

--- ESP32/Kyber/main/main.c
+++ ESP32/Kyber/main/main.c
@@ -6,6 +6,7 @@
 #include "main.h"
 #include "kem.h"
 #include "api.h"
+#include "esp_timer.h"
 
 #include "randombytes.h"
 
@@ -13,69 +14,90 @@ void app_main (void)
 {
     printf("Inicio con la opción %d\n\r", KYBER_K);
 
-    printf("Rand 1  = {46, 216, 222, 138, 243, 168, 235, 98, 147, 193, 144, 105, 109, 44, 224, 146, 06, 206, 244, 246, 240, 232, 157, 249, 172, 06, 16, 55, 142, 210, 18, 251}");
-    printf("Rand 2  = {179, 157, 148, 161, 216, 198, 30, 180, 183, 98, 150, 193, 60, 125, 68, 145, 152, 131, 43, 182, 212, 21, 155, 51, 77, 133, 52, 17, 19, 224, 249, 94}");
-    printf("Rand 3  = {9, 213, 140, 84, 62, 200, 241, 254, 53, 233, 77, 67, 53, 145, 45, 237, 67, 93, 47, 43, 166, 100, 211, 135, 174, 04, 149, 07, 205, 64, 91, 25}");
+    // printf("Rand 1  = {46, 216, 222, 138, 243, 168, 235, 98, 147, 193, 144, 105, 109, 44, 224, 146, 06, 206, 244, 246, 240, 232, 157, 249, 172, 06, 16, 55, 142, 210, 18, 251}");
+    // printf("Rand 2  = {179, 157, 148, 161, 216, 198, 30, 180, 183, 98, 150, 193, 60, 125, 68, 145, 152, 131, 43, 182, 212, 21, 155, 51, 77, 133, 52, 17, 19, 224, 249, 94}");
+    // printf("Rand 3  = {9, 213, 140, 84, 62, 200, 241, 254, 53, 233, 77, 67, 53, 145, 45, 237, 67, 93, 47, 43, 166, 100, 211, 135, 174, 04, 149, 07, 205, 64, 91, 25}");
 
     //Generación de claves
     unsigned char *pk = (unsigned char*) malloc(CRYPTO_PUBLICKEYBYTES * sizeof(unsigned char));
     unsigned char *sk = (unsigned char*) malloc(CRYPTO_SECRETKEYBYTES * sizeof(unsigned char));
 
-    if (crypto_kem_keypair(pk, sk) != 0) {
-        printf("Generacion de claves fallida\n\r");
-        free(pk);
-        free(sk);
-        return;
-    } else {
-        printf("Generacion de claves exitosa (%d bytes de clave publica y %d bytes de clave privada)\n\r", CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES);
-        printBstr("Clave publica = ", pk, CRYPTO_PUBLICKEYBYTES);
-        printBstr("Clave privada = ", sk, CRYPTO_SECRETKEYBYTES);
-    }
+    // if (crypto_kem_keypair(pk, sk) != 0) {
+    //     printf("Generacion de claves fallida\n\r");
+    //     free(pk);
+    //     free(sk);
+    //     return;
+    // } else {
+    //     printf("Generacion de claves exitosa (%d bytes de clave publica y %d bytes de clave privada)\n\r", CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES);
+    //     printBstr("Clave publica = ", pk, CRYPTO_PUBLICKEYBYTES);
+    //     printBstr("Clave privada = ", sk, CRYPTO_SECRETKEYBYTES);
+    // }
 
 
     //Generación de texto cifrado y secreto compartido
     unsigned char *ct = (unsigned char*) malloc(CRYPTO_CIPHERTEXTBYTES * sizeof(unsigned char));
     unsigned char *ss_pub = (unsigned char*) malloc(CRYPTO_BYTES * sizeof(unsigned char));
 
-    if (crypto_kem_enc(ct, ss_pub, pk) != 0) {
-        printf("Generacion de texto cifrado y secreto compartido fallida\n\r");
-        free(pk);
-        free(sk);
-        free(ct);
-        free(ss_pub);
-        return;
-    } else {
-        printf("Generacion de texto cifrado y secreto compartido exitosa (%d bytes de texto cifrado y %d bytes de secreto compartido)\n\r", CRYPTO_CIPHERTEXTBYTES, CRYPTO_BYTES);
-        printBstr("Texto cifrado = ", ct, CRYPTO_CIPHERTEXTBYTES);
-        // printBstr("Secreto compartido = ", ss_pub, CRYPTO_BYTES);
-    }
+    // if (crypto_kem_enc(ct, ss_pub, pk) != 0) {
+    //     printf("Generacion de texto cifrado y secreto compartido fallida\n\r");
+    //     free(pk);
+    //     free(sk);
+    //     free(ct);
+    //     free(ss_pub);
+    //     return;
+    // } else {
+    //     printf("Generacion de texto cifrado y secreto compartido exitosa (%d bytes de texto cifrado y %d bytes de secreto compartido)\n\r", CRYPTO_CIPHERTEXTBYTES, CRYPTO_BYTES);
+    //     printBstr("Texto cifrado = ", ct, CRYPTO_CIPHERTEXTBYTES);
+    //     // printBstr("Secreto compartido = ", ss_pub, CRYPTO_BYTES);
+    // }
 
 
     //Generación de secreto compartido
     unsigned char *ss_priv = (unsigned char*) malloc(CRYPTO_BYTES * sizeof(unsigned char));
 
-    if (crypto_kem_dec(ss_priv, ct, sk) != 0) {
-        printf("Generacion de secreto compartido fallida\n\r");
-        free(pk);
-        free(sk);
-        free(ct);
-        free(ss_pub);
-        free(ss_priv);
-        return;
-    } else {
-        printf("Generacion de secreto compartido exitosa (%d bytes de secreto compartido)\n\r", CRYPTO_BYTES);
-        printBstr("Secreto compartido pub  = ", ss_pub, CRYPTO_BYTES);
-        printBstr("Secreto compartido priv = ", ss_priv, CRYPTO_BYTES);
+    // if (crypto_kem_dec(ss_priv, ct, sk) != 0) {
+    //     printf("Generacion de secreto compartido fallida\n\r");
+    //     free(pk);
+    //     free(sk);
+    //     free(ct);
+    //     free(ss_pub);
+    //     free(ss_priv);
+    //     return;
+    // } else {
+    //     printf("Generacion de secreto compartido exitosa (%d bytes de secreto compartido)\n\r", CRYPTO_BYTES);
+    //     printBstr("Secreto compartido pub  = ", ss_pub, CRYPTO_BYTES);
+    //     printBstr("Secreto compartido priv = ", ss_priv, CRYPTO_BYTES);
+    // }
+
+
+    // //Comprobación de resultados
+    // if (memcmp(ss_pub, ss_priv, (unsigned int) CRYPTO_BYTES) ) {
+    //     printf("Comprobación fallida\n\r");
+    // } else{
+    //     printf("¡Comprobación superada!\n\r");
+    // }
+
+    printf("Gen, Enc, Dec\n\r");
+    uint64_t time_ini, time_end;
+
+    for(int c = 0; c < 1000; c++){
+        time_ini = esp_timer_get_time();
+        crypto_kem_keypair(pk, sk);
+        time_end = esp_timer_get_time();
+        printf("%llu,", time_end-time_ini);
+
+
+        time_ini = esp_timer_get_time();
+        crypto_kem_enc(ct, ss_pub, pk);
+        time_end = esp_timer_get_time();
+        printf("%llu,", time_end-time_ini);
+
+
+        time_ini = esp_timer_get_time();
+        crypto_kem_dec(ss_priv, ct, sk);
+        time_end = esp_timer_get_time();
+        printf("%llu\n\r", time_end-time_ini);
     }
-
-
-    //Comprobación de resultados
-    if (memcmp(ss_pub, ss_priv, (unsigned int) CRYPTO_BYTES) ) {
-        printf("Comprobación fallida\n\r");
-    } else{
-        printf("¡Comprobación superada!\n\r");
-    }
-
 
     //Liberación de la memoria reservada
     free(pk);
--- RP2040/Sphincs/main.c
+++ RP2040/Sphincs/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "api.h"
 
 
@@ -24,6 +25,7 @@ printBstr(char *S, unsigned char *A, unsigned long long L)
 
 int main() {
     stdio_init_all();
+    sleep_ms(10000);
     printf("Inicio con la opción %s\n\r", xstr(PARAMS));
 
     //Generación de claves
@@ -38,49 +40,81 @@ int main() {
     unsigned char *m1;
     unsigned long long mlen1;
 
+    absolute_time_t time_ini, time_end;
+
     while (true) {
 
-        //Generación de claves
-        printf("Inicio\n");
-        if (crypto_sign_keypair(pk, sk)!= 0){
-            printf("Generacion de claves fallida\n\r");
-        } else {
-            printf("Generacion de claves exitosa (%d bytes de clave publica y %d bytes de clave privada)\n\r", CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES);
-            // printBstr("Clave publica = ", pk, CRYPTO_PUBLICKEYBYTES);
-            // printBstr("Clave privada = ", sk, CRYPTO_SECRETKEYBYTES);
+        // //Generación de claves
+        // printf("Inicio\n");
+        // if (crypto_sign_keypair(pk, sk)!= 0){
+        //     printf("Generacion de claves fallida\n\r");
+        // } else {
+        //     printf("Generacion de claves exitosa (%d bytes de clave publica y %d bytes de clave privada)\n\r", CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES);
+        //     // printBstr("Clave publica = ", pk, CRYPTO_PUBLICKEYBYTES);
+        //     // printBstr("Clave privada = ", sk, CRYPTO_SECRETKEYBYTES);
+        // }
+
+
+        // //Firma de mensaje
+        // m1 = (unsigned char *)calloc(mlen+CRYPTO_BYTES, sizeof(unsigned char));
+        // sm = (unsigned char *)calloc(mlen+CRYPTO_BYTES, sizeof(unsigned char));
+
+        // if (crypto_sign(sm, &smlen, m, mlen, sk) != 0){
+        //     printf("Firma de mensaje fallida\n\r");
+        // } else {
+        //     printf("Firma del mensaje exitosa (%llu bytes de firma)\n\r", smlen);
+        //     // printBstr("Firma del mensaje = ", sm, smlen);
+        // }
+
+
+        // //Comprobación de la firma
+        // if (crypto_sign_open(m1, &mlen1, sm, smlen, pk) != 0) {
+        //     printf("Comprobacion de la firma fallida\n\r");
+        // } else {
+        //     if (mlen != mlen1) {
+        //         printf("Longitud del mensaje original distinta a la longitud del mensaje recuperado\n\r");
+        //     } else if (memcmp(m, m1, mlen)) {
+        //         printf("Contenido del mensaje original distinto al contenido del mensaje recuperado\n\r");
+        //     } else {
+        //         printf("Comprobacion de la firma del mensaje exitosa (%llu bytes de mensaje)\n\r", mlen1);
+        //         printf("Comprobacion de la firma del mensaje = \"%s\"\n\r", m1);
+        //     }
+        // }
+
+
+        //printf("Inicio\n");
+        printf("Gen, Sign, Ver\n\r");
+
+        for(int c = 0; c<100; c++){
+
+            time_ini = get_absolute_time();
+            crypto_sign_keypair(pk, sk);
+            time_end = get_absolute_time();
+            printf("%llu, ", time_end-time_ini);
+
+
+            //Firma de mensaje
+            m1 = (unsigned char *)calloc(mlen+CRYPTO_BYTES, sizeof(unsigned char));
+            sm = (unsigned char *)calloc(mlen+CRYPTO_BYTES, sizeof(unsigned char));
+
+            time_ini = get_absolute_time();
+            crypto_sign(sm, &smlen, m, mlen, sk);
+            time_end = get_absolute_time();
+            printf("%llu, ", time_end-time_ini);
+
+
+            //Comprobación de la firma
+            time_ini = get_absolute_time();
+            crypto_sign_open(m1, &mlen1, sm, smlen, pk);
+            time_end = get_absolute_time();
+            printf("%llu\n\r", time_end-time_ini);
+
+
+            //Liberación de la memoria reservada
+            free(m1);
+            free(sm);
         }
 
-
-        //Firma de mensaje
-        m1 = (unsigned char *)calloc(mlen+CRYPTO_BYTES, sizeof(unsigned char));
-        sm = (unsigned char *)calloc(mlen+CRYPTO_BYTES, sizeof(unsigned char));
-
-        if (crypto_sign(sm, &smlen, m, mlen, sk) != 0){
-            printf("Firma de mensaje fallida\n\r");
-        } else {
-            printf("Firma del mensaje exitosa (%llu bytes de firma)\n\r", smlen);
-            // printBstr("Firma del mensaje = ", sm, smlen);
-        }
-
-
-        //Comprobación de la firma
-        if (crypto_sign_open(m1, &mlen1, sm, smlen, pk) != 0) {
-            printf("Comprobacion de la firma fallida\n\r");
-        } else {
-            if (mlen != mlen1) {
-                printf("Longitud del mensaje original distinta a la longitud del mensaje recuperado\n\r");
-            } else if (memcmp(m, m1, mlen)) {
-                printf("Contenido del mensaje original distinto al contenido del mensaje recuperado\n\r");
-            } else {
-                printf("Comprobacion de la firma del mensaje exitosa (%llu bytes de mensaje)\n\r", mlen1);
-                printf("Comprobacion de la firma del mensaje = \"%s\"\n\r", m1);
-            }
-        }
-
-
-        //Liberación de la memoria reservada
-        free(m1);
-        free(sm);
-        sleep_ms(10000);
+        sleep_ms(30000);
     }
 }
